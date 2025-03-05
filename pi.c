c211f9a2f8d8f7ff1368829730061201b0da5c51
lucaszrhill <hill5240@mylaurier.ca>
2025-03-04 22:35:12 -0500

    Update pi.c

--- pi.c
+++ pi.c
@@ -4,21 +4,23 @@
 #include <math.h>
 #include <pthread.h>
 
-const int num_of_Points = 50000000;
-const int num_of_Threads = 4; // Set a fixed number of threads
 int points_in_circle = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-double random_double() {
-	return rand() / (double) (RAND_MAX + 1);
+double random_double(struct drand48_data *buffer) {
+	double result;
+	drand48_r(buffer, &result);
+	return result;
 }
 
 void* generate_points(void *arg) {
 	int points = *(int*) arg;
+	struct drand48_data buffer;
+	srand48_r(time(NULL) ^ pthread_self(), &buffer); // Unique seed for each thread
 	int count = 0;
 	for (int i = 0; i < points; ++i) {
-		double x = random_double() * 2.0 - 1.0;
-		double y = random_double() * 2.0 - 1.0;
+		double x = random_double(&buffer) * 2.0 - 1.0;
+		double y = random_double(&buffer) * 2.0 - 1.0;
 		if (sqrt(x * x + y * y) <= 1.0) {
 			++count;
 		}
@@ -26,10 +28,31 @@ void* generate_points(void *arg) {
 	pthread_mutex_lock(&mutex);
 	points_in_circle += count;
 	pthread_mutex_unlock(&mutex);
+	printf("Thread completed. Points in circle: %d\n", points_in_circle); // Debug print
 	return NULL;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		printf(
+				"Usage: %s <integer value for NUMBER_OF_DARTS> <integer value for NUMBER_OF_THREADS>\n",
+				argv[0]);
+		return 1;
+	}
+
+	int num_of_Points = atoi(argv[1]);
+	int num_of_Threads = atoi(argv[2]);
+
+	if (num_of_Points < 5000000) {
+		printf("The number of darts must be >= 5000000\n");
+		return 1;
+	}
+
+	if (num_of_Threads < 2) {
+		printf("The number of threads must be >= 2\n");
+		return 1;
+	}
+
 	srand((unsigned int) time(NULL));
 
 	pthread_t threads[num_of_Threads];
@@ -44,7 +67,7 @@ int main() {
 	}
 
 	double pi = 4.0 * points_in_circle / num_of_Points;
-	printf("Estimated value of Pi: %lf\n", pi);
+	printf("Pi = %lf\n", pi);
 
 	pthread_mutex_destroy(&mutex);
 	return 0;
